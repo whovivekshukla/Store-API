66669d90a993d4c12274c2e5911774942437e337
whovivekshukla <vivekshuklahere@gmail.com>
2022-12-28 17:09:03 +0530

    Update

---
+++ app.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+require("express-async-errors")
+const express = require("express");
+const app = express();
+
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
+
+const notFoundMiddleWare = require("./middleware/not-found");
+const errorMiddleWare = require("./middleware/error-handler");
+
+//middleware
+
+app.use(express.json({}));
+
+app.get("/", (req, res) => {
+  res.send("<h1>Store API</h1> <a href='/api/v1/products'>Products Route</a>");
+});
+
+app.use("/api/v1/products", productsRouter);
+
+// products route
+
+app.use(notFoundMiddleWare);
+app.use(errorMiddleWare);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening at ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
